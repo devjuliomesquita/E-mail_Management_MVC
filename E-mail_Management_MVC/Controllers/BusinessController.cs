9d401d899f7d2b4890dc50139f0d01545b601a18
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-08 20:19:33 -0300

    Adicionar arquivos de projeto.

---
+++ E-mail_Management_MVC/Controllers/BusinessController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_mail_Management_MVC.Controllers
+{
+    public class BusinessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
