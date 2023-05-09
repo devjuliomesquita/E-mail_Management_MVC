030d9da22965ec44f50b3d788089b5b3330b9258
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-08 22:45:09 -0300

    Criado a biblioteca de classes Domain Criado duas Entidades Business e Email

---
+++ E-mail_Management_Domain/Entities/Business.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_mail_Management_Domain.Entities
+{
+    public class Business
+    {
+        public Business(string? businessName)
+        {
+            BusinessName = businessName;
+
+            BusinessNNI = false;
+            Active = true;
+            CreatedAt = DateTime.Now;
+            Emails = new List<BusinessEmail>();
+        }
+
+        public int BusinessId { get; private set; }
+        public string? BusinessName { get; private set; }
+        public bool BusinessNNI { get; private set; }
+        public bool Active { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime CanceledAt { get; private set; }
+        public List<BusinessEmail>? Emails { get; private set; }
+    }
+}
---
+++ E-mail_Management_Domain/Entities/BusinessEmail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_mail_Management_Domain.Entities
+{
+    public class BusinessEmail
+    {
+        public BusinessEmail(string? email, int businessId)
+        {
+            Email = email;
+            BusinessId = businessId;
+        }
+
+        public int BusinessEmailId { get; private set; }
+        public string? Email { get; private set; }
+        public int BusinessId { get; private set; }
+    }
+}
